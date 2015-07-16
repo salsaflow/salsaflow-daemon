4d75c6534762920a79814bd1bc56dbd7ecf2c4f5
Ondřej Kupka <ondra.cap@gmail.com>
2015-07-16 16:51:42 +0200

    wip [ci skip]
    Change-Id: 7bf94fb267

--- Godeps/_workspace/src/github.com/salsaflow/salsaflow/github/issues/review_issue.go
+++ Godeps/_workspace/src/github.com/salsaflow/salsaflow/github/issues/review_issue.go
@@ -57,18 +57,39 @@ func FindIssueForCommitItem(
 ) (*github.Issue, error) {
 
 	// Find the relevant review issue.
+	// We need to iterate since the result is paginated.
 	pattern := fmt.Sprintf("] %v: %v", commitSHA[:7], commitTitle)
 
 	query := fmt.Sprintf(
 		`"%v" repo:"%v/%v" label:review type:issue in:body`, pattern, owner, repo)
 
-	result, _, err := client.Search.Issues(query, &github.SearchOptions{})
-	if err != nil {
-		return nil, err
+	searchOpts := &github.SearchOptions{}
+	searchOpts.Page = 1
+	searchOpts.PerPage = 50
+
+	var searched int
+
+	for {
+		// Fetch another page.
+		result, _, err := client.Search.Issues(query, searchOpts)
+		if err != nil {
+			return nil, err
+		}
+
+		// Check the issues for exact string match.
+		for _, issue := range result.Issues {
+			if strings.Contains(*issue.Body, pattern) {
+				return &issue, nil
+			}
+		}
+
+		// Check whether we have reached the end or not.
+		searched += len(result.Issues)
+		if searched == *result.Total {
+			return nil, &ErrReviewIssueNotFound{commitSHA, result}
+		}
+
+		// Check the next page in the next iteration.
+		searchOpts.Page += 1
 	}
-	if total := *result.Total; total != 1 {
-		return nil, &ErrReviewIssueNotFound{commitSHA, result}
-	}
-	issue := result.Issues[0]
-	return &issue, nil
 }
